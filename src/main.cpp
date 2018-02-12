ebeb98b716ed1b1f6280b25fcc76c427ba2c2af9
Antoine <ameisso@gmail.com>
2018-02-12 20:10:17 +0100

    New : first rotation

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,36 @@
 #include "main.h"
 
+
 void setup()
- {
-  //setupHardware();
+{
+  setupHardware();
   Serial.begin(115200);
-  DEBUG_PRINTLN("INIT OK ");
+
 }
-void loop() {
-  for (int pos = 0; pos <= maxSpeed; pos += 1)
+
+
+void loop()
+{
+  for (pos = 0; pos <= 180; pos += 1)
   {
     analogWrite(PWM_OUT,pos);
+    DEBUG_PRINTLN(pos);
     delay(delaySpeed);
     digitalWrite(LED_BUILTIN,ledState);
     ledState = ! ledState;
+  }
+  for (pos = 180; pos >= 0; pos -= 1)
+  {
+    analogWrite(PWM_OUT,pos);
     DEBUG_PRINTLN(pos);
+    delay(delaySpeed);
+    digitalWrite(LED_BUILTIN,ledState);
+    ledState = ! ledState;
   }
 }
 
 void setupHardware()
 {
   pinMode(PWM_OUT,OUTPUT);
+  pinMode(LED_BUILTIN,OUTPUT);
 }
--- src/main.h
+++ src/main.h
@@ -1,13 +1,14 @@
 #include <Arduino.h>
 
+#define DEBUG
 #define PWM_OUT 3
 
 void setupHardware();
 
-
 int delaySpeed = 300;
 int maxSpeed = 100;
 bool ledState = true;
+int pos = 0; 
 
 #ifdef DEBUG
 #define DEBUG_PRINT(x)  Serial.print (x)
