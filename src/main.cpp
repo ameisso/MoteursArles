3106ab80d3eb147c148bb4064cd130e5b808099f
Antoine <ameisso@gmail.com>
2018-02-12 22:45:06 +0100

    New : read coder

--- src/main.cpp
+++ src/main.cpp
@@ -5,32 +5,26 @@ void setup()
 {
   setupHardware();
   Serial.begin(115200);
-
 }
 
 
 void loop()
 {
-  for (pos = 0; pos <= 180; pos += 1)
-  {
-    analogWrite(PWM_OUT,pos);
-    DEBUG_PRINTLN(pos);
-    delay(delaySpeed);
-    digitalWrite(LED_BUILTIN,ledState);
-    ledState = ! ledState;
-  }
-  for (pos = 180; pos >= 0; pos -= 1)
-  {
-    analogWrite(PWM_OUT,pos);
-    DEBUG_PRINTLN(pos);
-    delay(delaySpeed);
-    digitalWrite(LED_BUILTIN,ledState);
-    ledState = ! ledState;
-  }
+  analogWrite(PWM_OUT,0);
+  DEBUG_PRINTLN(getCoderTurns());
+  delay(delaySpeed);
+  digitalWrite(LED_BUILTIN,ledState);
+  ledState = ! ledState;
 }
 
 void setupHardware()
 {
   pinMode(PWM_OUT,OUTPUT);
   pinMode(LED_BUILTIN,OUTPUT);
+  coder.write(0);
+}
+
+float getCoderTurns()
+{
+  return coder.read()/CODER_RESOLUTION/4.0;
 }
--- src/main.h
+++ src/main.h
@@ -1,14 +1,18 @@
 #include <Arduino.h>
+#include <Encoder.h>
 
 #define DEBUG
 #define PWM_OUT 3
+#define CODER_A 2
+#define CODER_B 4
+#define CODER_RESOLUTION 600
+
 
 void setupHardware();
-
+float getCoderTurns();
+Encoder coder(CODER_A, CODER_B);
 int delaySpeed = 300;
-int maxSpeed = 100;
 bool ledState = true;
-int pos = 0; 
 
 #ifdef DEBUG
 #define DEBUG_PRINT(x)  Serial.print (x)
