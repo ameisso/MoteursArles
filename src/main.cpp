41d058886b7bf8144271569fabb2bdd2bde8125a
Antoine <ameisso@gmail.com>
2018-02-13 01:27:15 +0100

    New : coder and command

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,49 @@ void setup()
 
 void loop()
 {
-  analogWrite(PWM_OUT,0);
-  DEBUG_PRINTLN(getCoderTurns());
-  delay(delaySpeed);
-  digitalWrite(LED_BUILTIN,ledState);
-  ledState = ! ledState;
+  char speed = getSerialSpeed();
+  if(speed >= 0)
+  {
+    DEBUG_PRINT("SPEED: ");
+    DEBUG_PRINTLN_CAST (speed,DEC);
+    analogWrite(PWM_OUT,speed);
+    delay(delaySpeed);
+    DEBUG_PRINTLN(getCoderTurns());
+    digitalWrite(LED_BUILTIN,ledState);
+    ledState = ! ledState;
+  }
 }
 
 void setupHardware()
 {
   pinMode(PWM_OUT,OUTPUT);
   pinMode(LED_BUILTIN,OUTPUT);
+  analogWrite(PWM_OUT,0);
   coder.write(0);
 }
 
 float getCoderTurns()
 {
   return coder.read()/CODER_RESOLUTION/4.0;
+}
+
+int getSerialSpeed()
+{
+  if (Serial.available() > 0)
+  {
+    int incomingByte = 0;
+    incomingByte = Serial.read();
+    if (incomingByte != 97)
+    {
+      incomingByte  = map(incomingByte, 97, 122, MIN_SPEED, 255);
+    }
+    else
+    {
+      incomingByte = 0;
+    }
+    DEBUG_PRINT("I received: ");
+    DEBUG_PRINTLN_CAST (incomingByte,DEC);
+    return incomingByte;
+  }
+  return -1;
 }
--- src/main.h
+++ src/main.h
@@ -6,18 +6,24 @@
 #define CODER_A 2
 #define CODER_B 4
 #define CODER_RESOLUTION 600
+#define MIN_SPEED 81
 
+
+int delaySpeed = 2000;
 
 void setupHardware();
 float getCoderTurns();
+int getSerialSpeed();
 Encoder coder(CODER_A, CODER_B);
-int delaySpeed = 300;
+
 bool ledState = true;
 
 #ifdef DEBUG
 #define DEBUG_PRINT(x)  Serial.print (x)
 #define DEBUG_PRINTLN(x)  Serial.println (x)
+#define DEBUG_PRINTLN_CAST(x,y)  Serial.println (x,y)
 #else
 #define DEBUG_PRINT(x)
 #define DEBUG_PRINTLN(x)
+#define DEBUG_PRINTLN_CAST(x,y)
 #endif
